28db240153eea006991daf32a9b9dc43ce0a3c23
jcha40 <sungsoocha97@gmail.com>
2025-02-11 15:34:57 -0500

    Added CLI

---
+++ clamp-python/run_clamp.py
@@ -0,0 +1,87 @@
+import re
+import numpy as np
+import os
+from engine import GreedyEngine, GreedyItem
+from utils import trim_motif, plot_logo_stack
+import argparse
+
+def parse_meme_files(meme_files, nsites_thresh=10, evalue_thresh=.1,
+    nsites_pattern = re.compile('letter-probability matrix: alength= \d+ w= \d+ nsites= (\d+) E= ([\d.+e-]+)'),
+    weights_pattern = re.compile('\s*([\d\.]+)\s*([\d\.]+)\s*([\d\.]+)\s*([\d\.]+)')):
+
+    items = []
+    sources = []
+    n = 0
+    for fn in meme_files:
+        with open(fn, 'r') as f:
+            for line in f:
+                nsites_match = nsites_pattern.match(line)
+                if nsites_match:
+                    nsites = int(nsites_match.groups()[0])
+                    evalue = float(nsites_match.groups()[1])
+                    rows = []
+                    line = f.readline()
+                    weights_match = weights_pattern.match(line)
+                    while weights_match:
+                        rows.append([float(w) * nsites for w in weights_match.groups()])
+                        line = f.readline()
+                        weights_match = weights_pattern.match(line)
+                    if nsites >= nsites_thresh and evalue <= evalue_thresh:
+                        items.append(GreedyItem(n, np.array(rows)))
+                        sources.append(('{}-motif{}'.format(fn, n), nsites, evalue))
+                        n += 1
+    
+    return items, sources
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run CLAMP on a set of MEME files')
+    parser.add_argument('--meme', nargs='+', default=None, help='MEME files to process')
+    parser.add_argument('--meme-list', default=None, help='File containing list of MEME files to process')
+    parser.add_argument('--nsites-thresh', type=int, default=10, help='Minimum number of sites to consider a motif')
+    parser.add_argument('--evalue-thresh', type=float, default=.1, help='Maximum E-value to consider a motif')
+    parser.add_argument('--pc', nargs=4, type=float, default=[1., 1., 1., 1.], help='Prior counts for PWM')
+    parser.add_argument('--min-information-overlap', type=float, default=8., help='Minimum information overlap for merging')
+    parser.add_argument('--max-information-overhang', type=float, default=12., help='Maximum information overhang for merging')
+    parser.add_argument('--concentration', type=float, default=1., help='Concentration parameter for merging')
+    parser.add_argument('--n-processes', type=int, default=None, help='Number of processes to use for parallelization')
+    parser.add_argument('--info-thresh', type=float, default=.5, help='Information threshold for trimming motifs')
+    parser.add_argument('--output-dest', '-o', default='clamp_out', help='Folder to save results, will be created if it does not exist')
+    args = parser.parse_args()
+
+    if not os.path.exists(args.output_dest):
+        os.makedirs(args.output_dest)
+    if not os.path.exists('{}/svgs'.format(args.output_dest)):
+        os.mkdir('{}/svgs'.format(args.output_dest))
+
+    if args.meme:
+        if args.meme_list:
+            print('Warning: both --meme and --meme-list specified, ignoring --meme-list')
+        meme_files = args.meme
+    elif args.meme_list:
+        with open(args.meme_list, 'r') as f:
+            meme_files = [line.strip() for line in f]
+    else:
+        parser.error('Either --meme or --meme-list must be specified')
+    
+    items, sources = parse_meme_files(meme_files, args.nsites_thresh, args.evalue_thresh)
+    engine = GreedyEngine(items, pc=args.pc, min_information_overlap=args.min_information_overlap,
+        max_information_overhang=args.max_information_overhang, concentration=args.concentration)
+    engine.cluster_motifs(n_processes=args.n_processes)
+
+    maximal_clusters = engine.clusters_trace[np.argmax(engine.llr_trace)]
+    with open('{}/matrices.chen'.format(args.output_dest), 'w') as chen:
+        with open('{}/motif_clusters.txt'.format(args.output_dest), 'w') as txt:
+            for c in sorted(maximal_clusters, key=lambda c: len(engine.clusters[c].items), reverse=True):
+                cluster = engine.clusters[c]
+
+                trimmed_pwm = trim_motif(cluster.aligned_pwms, args.info_thresh)
+                chen.write('>motif{}\n'.format(c))
+                for row in trimmed_pwm:
+                    chen.write('{:.06f}\t{:.06f}\t{:.06f}\t{:.06f}\n'.format(*row))
+
+                txt.write('motif{}\t{}\n'.format(c, '\t'.join([sources[item.idx][0] for item in cluster.items])))
+
+                svg = plot_logo_stack(cluster.aligned_pwms)
+                with open('{}/svgs/motif{}.svg'.format(args.output_dest, c), 'w') as f:
+                    svg.writexml(f, addindent='\t', newl='\n')
--- clamp-python/utils.py
+++ clamp-python/utils.py
@@ -1,6 +1,91 @@
 import numpy as np
+import xml.dom.minidom as dom
 from typing import Union
 
 def boltzmann(arr: np.ndarray, alpha: float, axis: Union[None, int, tuple] = None):
     return np.sum(arr * np.exp(alpha * arr), axis=axis) / np.sum(np.exp(alpha * arr), axis=axis)
     
+    
+def trim_motif(aligned_pwms, info_thresh=.5):
+    pwm = np.sum(aligned_pwms, axis=0)
+
+    if aligned_pwms.shape[1] == 1:
+        return pwm
+
+    posterior_pwm = (pwm + 1.) / (np.sum(pwm, axis=1, keepdims=True) + 4.)
+    bits = np.sum(posterior_pwm * np.log2(posterior_pwm), axis=1) + 2.
+    informative_bits = np.flatnonzero(bits > info_thresh)
+    start = informative_bits[0]
+    end = informative_bits[-1] + 1
+    return pwm[start:end, :]
+
+
+class DNASymbol:
+    path = None
+    color = None
+    max_bits = 2
+    DNA_alphabet = ()
+
+    @classmethod
+    def get_symbol(cls, i):
+        return cls.DNA_alphabet[i]
+
+class DNA_A(DNASymbol):
+    path = 'M 0 100 L 33 0 L 66 0 L 100 100 L 75 100 L 66 75 L 33 75 L 25 100 L 0 100 M 41 55 L 58 55 L 50 25 L 41 55'
+    color = '#FF0000'
+
+class DNA_C(DNASymbol):
+    path = 'M 100 28 C 100 -13 0 -13 0 50 C 0 113 100 113 100 72 L 75 72 C 75 90 30 90 30 50 C 30 10 75 10 75 28 Z'
+    color = '#0000FF'
+
+class DNA_G(DNASymbol):
+    path = 'M 100 28 C 100 -13 0 -13 0 50 C 0 113 100 113 100 72 L 100 48 L 55 48 L 55 72 L 75 72 C 75 90 30 90 30 50 C 30 10 75 5 75 28 Z'
+    color = '#FFA500'
+
+class DNA_T(DNASymbol):
+    path = 'M 0 0 L 0 20 L 35 20 L 35 100 L 65 100 L 65 20 L 100 20 L 100 0 Z'
+    color = '#228B22'
+
+DNASymbol.DNA_alphabet = (DNA_A, DNA_C, DNA_G, DNA_T)
+
+
+def plot_logo_stack(aligned_pwms, symbol=DNASymbol, glyph_width=100, stack_height=200):
+    height, width, _ = aligned_pwms.shape
+
+    document = dom.Document()
+    svg = document.appendChild(document.createElement('svg'))
+    svg.setAttribute('baseProfile', 'full')
+    svg.setAttribute('version', '1.1')
+    svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
+    svg.setAttribute('viewBox', '0 0 {} {}'.format(width * glyph_width, height * stack_height))
+
+    logo_stack = svg.appendChild(document.createElement('g'))
+
+    for y, pwm in enumerate(aligned_pwms):
+        row = logo_stack.appendChild(document.createElement('g'))
+        row.setAttribute('transform', 'translate(0 {})'.format(y * stack_height))
+        for i, pwv in enumerate(pwm):
+            n = np.sum(pwv)
+            if n == 0:
+                continue
+            vec = pwv / n
+            vec[vec > 0] *= np.log2(vec[vec > 0])
+            bits = sum(vec, symbol.max_bits)
+            heights = pwv / n * bits / symbol.max_bits * stack_height
+            idx = np.argsort(pwv)
+    
+            stack = row.appendChild(document.createElement('g'))
+            stack.setAttribute('transform', 'translate({} 0)'.format(i * glyph_width))
+    
+            y_offset = 0
+            for j in idx:
+                if heights[j] == 0:
+                    continue
+                base = symbol.get_symbol(j)
+                y_offset += heights[j]
+                glyph = stack.appendChild(document.createElement('path'))
+                glyph.setAttribute('d', base.path)
+                glyph.setAttribute('fill', base.color)
+                glyph.setAttribute('transform', 'matrix({} 0 0 {} 0 {})'.format(glyph_width / 100., heights[j] / 100., stack_height - y_offset))
+
+    return svg
