630538fc184951973a45cefe95717c5e6145b395
jcha40 <sungsoocha97@gmail.com>
2025-05-15 13:38:19 -0400

    Added scripts for motif simulation

---
+++ simulate-motifs/simulate_base_pwms.py
@@ -0,0 +1,141 @@
+import numpy as np
+from scipy.special import digamma
+import argparse
+
+a = np.logspace(-10, 3, 1001)
+h_hat = digamma(4 * a + 1) - digamma(a + 1)
+
+def simulate_base_pwms_default(entr_dist, n=100, min_width=6, max_width=30,
+                               min_logits=5., bg_alpha=100., rng=np.random.default_rng()):
+    pwms = []
+    
+    widths = rng.integers(min_width, max_width + 1, size=n)
+    left_pad = (max_width - widths) // 2
+    right_pad = max_width - widths - left_pad
+    for i in range(n):
+        entr = rng.choice(entr_dist, size=widths[i])
+        if sum(np.log(4) - entr) < min_logits:
+            entr *= (widths[i] * np.log(4) - min_logits) / np.sum(entr)
+        alpha = np.insert(np.interp(entr, h_hat, a), [0] * left_pad[i] + [widths[i]] * right_pad[i], bg_alpha)
+        
+        pwms.append(np.stack([rng.dirichlet([alpha[j]] * 4) for j in range(max_width)]))
+    return pwms
+
+def simulate_base_pwms_palindromic(entr_dist, n=100, min_width=6, max_width=30,
+                                   min_logits=5., bg_alpha=100., rng=np.random.default_rng()):
+    pwms = []
+    
+    widths = rng.integers(min_width, max_width + 1, size=n)
+    left_pad = (max_width - widths) // 2
+    for i in range(n):
+        halfwidth = int(np.ceil(widths[i] / 2))
+        entr = rng.choice(entr_dist, size=halfwidth)
+        entr_ = np.concatenate([entr, np.flip(entr)])[:widths[i]]
+        if sum(np.log(4) - entr_) < min_logits:
+            entr *= (widths[i] * np.log(4) - min_logits) / np.sum(entr_)
+        alpha = np.interp(entr, h_hat, a)
+        
+        half_pwm = np.empty((halfwidth, 4))
+        for j in range(halfwidth):
+            half_pwm[j, :] = rng.dirichlet([alpha[j]] * 4)
+        pwm = np.zeros((max_width, 4), dtype=np.float64)
+        pwm[left_pad[i]:left_pad[i] + widths[i], :] = np.concatenate([half_pwm, np.flip(half_pwm)])[:widths[i], :]
+        for j in range(left_pad[i]):
+            pwm[j, :] = rng.dirichlet([bg_alpha] * 4)
+        for j in range(left_pad[i] + widths[i], max_width):
+            pwm[j, :] = rng.dirichlet([bg_alpha] * 4)
+        pwms.append(pwm)
+    return pwms
+
+def simulate_base_pwms_periodic(entr_dist, n=100, min_width=6, max_width=30,
+                                min_period_width=4, max_period_width=6, min_logits=2.,
+                                min_period_stability=10., max_period_stability=100.,
+                                rng=np.random.default_rng()):
+    pwms = []
+    
+    period_widths = rng.integers(min_period_width, max_period_width + 1, size=n)
+    n_repeats = rng.integers(np.maximum(2, min_width // period_widths), max_width // period_widths)
+    n_before = rng.integers(0, n_repeats)
+    stability_weights = 1 / np.arange(min_period_stability, max_period_stability + 1)
+    stability_weights /= np.sum(stability_weights)
+    period_stability = rng.choice(np.arange(min_period_stability, max_period_stability + 1),
+                                  p=stability_weights, size=n)
+    for i in range(n):
+        entr = rng.choice(entr_dist, size=period_widths[i])
+        while sum(np.log(4) - entr) < min_logits:
+            entr = rng.choice(entr_dist, size=period_widths[i])
+        alpha = np.interp(entr, h_hat, a)
+        pc_mat = np.empty((period_widths[i], 4))
+        for j in range(period_widths[i]):
+            p = rng.dirichlet([alpha[j]] * 4)
+            pc_mat[j, :] = rng.multinomial(period_stability[i], p)
+
+        pwm = np.empty((period_widths[i] * n_repeats[i], 4))
+        for j in range(period_widths[i] * n_repeats[i]):
+            pc = abs(j // period_widths[i] - n_before[i])
+            mat_idx = j % period_widths[i]
+            pwm[j, :] = rng.dirichlet(pc_mat[mat_idx, :] + pc)
+        pwms.append(pwm)
+    return pwms, period_widths
+
+def write_pwm(motif_id, pwm, out_fh, period=None):
+    if period is None:
+        period = 0
+    out_fh.write('AC\t{}\n'.format(motif_id))
+    out_fh.write('XX\n')
+    out_fh.write('ID\t{}\n'.format(motif_id))
+    out_fh.write('PO	A	C	G	T\n')
+    for i in range(pwm.shape[0]):
+        out_fh.write('{:02d}\t{:06f}\t{:06f}\t{:06f}\t{:06f}\n'.format(i + 1, *pwm[i, :]))
+    out_fh.write('XX\n')
+    out_fh.write('PD\t{}\n'.format(period))
+    out_fh.write('XX\n//\n')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Simulate base PWMs')
+    parser.add_argument('--entr-dist', type=str, default=None)
+    parser.add_argument('--n', type=int, default=100)
+    parser.add_argument('--a-default', type=float, default=9.)
+    parser.add_argument('--a-palindromic', type=float, default=2.)
+    parser.add_argument('--a-periodic', type=float, default=2.)
+    parser.add_argument('--min-width', type=int, default=6)
+    parser.add_argument('--max-width', type=int, default=30)
+    parser.add_argument('--min-logits', type=float, default=5.)
+    parser.add_argument('--bg-alpha', type=float, default=100.)
+    parser.add_argument('--min-period-width', type=int, default=4)
+    parser.add_argument('--max-period-width', type=int, default=6)
+    parser.add_argument('--min-period-logits', type=float, default=2.)
+    parser.add_argument('--min-period-stability', type=float, default=10.)
+    parser.add_argument('--max-period-stability', type=float, default=100.)
+    parser.add_argument('--seed', type=int, default=None)
+    parser.add_argument('--out', type=str, default='base_pwms.transfac')
+    args = parser.parse_args()
+
+    rng = np.random.default_rng(args.seed)
+    entr_dist = np.loadtxt(args.entr_dist)
+    type_props = rng.dirichlet([args.a_default, args.a_palindromic, args.a_periodic])
+    n_def, n_pal, n_per = rng.multinomial(args.n, type_props)
+    
+    i = 1
+    with open(args.out, 'w') as f:
+        for pwm in simulate_base_pwms_default(entr_dist, n=n_def, min_width=args.min_width,
+            max_width=args.max_width, min_logits=args.min_logits, bg_alpha=args.bg_alpha, rng=rng):
+
+            write_pwm('motif{}'.format(i), pwm, f)
+            i += 1
+
+        for pwm in simulate_base_pwms_palindromic(entr_dist, n=n_pal, min_width=args.min_width,
+            max_width=args.max_width, min_logits=args.min_logits, bg_alpha=args.bg_alpha, rng=rng):
+
+            write_pwm('motif{}'.format(i), pwm, f)
+            i += 1
+        
+        for pwm, period in zip(*simulate_base_pwms_periodic(entr_dist, n=n_per,
+            min_width=args.min_width, max_width=args.max_width,
+            min_period_width=args.min_period_width, max_period_width=args.max_period_width,
+            min_logits=args.min_period_logits, min_period_stability=args.min_period_stability,
+            max_period_stability=args.max_period_stability, rng=rng)):
+
+            write_pwm('motif{}'.format(i), pwm, f, period=period)
+            i += 1
+            
---
+++ simulate-motifs/simulate_motifs.py
@@ -0,0 +1,124 @@
+import numpy as np
+import argparse
+from CLAMP.parse_tf import parse_tf
+
+def write_pfm(motif_id, pwm, nsites, forward_offset, reverse_offset, rc, out_fh,
+              rng=np.random.default_rng()):
+    out_fh.write('AC\t{}\n'.format(motif_id))
+    out_fh.write('XX\n')
+    out_fh.write('ID\t{}\n'.format(motif_id))
+    out_fh.write('PO	A	C	G	T\n')
+    for j in range(pwm.shape[0]):
+        out_fh.write('{:02d}\t{}\t{}\t{}\t{}\n'.format(j + 1, *rng.multinomial(nsites, pwm[j, :])))
+    out_fh.write('XX\n')
+    out_fh.write('FO\t{}\n'.format(forward_offset))
+    out_fh.write('RO\t{}\n'.format(reverse_offset))
+    out_fh.write('RC\t{}\n'.format(int(rc)))
+    out_fh.write('XX\n//\n')
+
+def simulate_aperiodic_motifs(base_motif_id, pwm, out_fh, exp_n=5., min_width=4, min_sites=20,
+                              max_sites=100, rng=np.random.default_rng()):
+    entr = pwm.copy()
+    entr[entr > 0] *= np.log2(entr[entr > 0])
+    p = -np.sum(entr, axis=1) / 2 + 1e-10
+    forward_weights = np.cumsum(np.log(np.insert(p, 0, 1.))) + np.log1p(np.append(-p, 0.))
+    reverse_weights = np.cumsum(np.log(np.insert(np.flip(p), 0, 1.))) + \
+        np.log1p(np.append(-np.flip(p), 0.))
+    
+    n = rng.poisson(exp_n)
+    rc = rng.binomial(1, .5, size=n).astype(bool)
+    nsites = rng.integers(min_sites, max_sites + 1, size=n)
+
+    i = 0
+    while i < n:
+        motif_id = '{}-{}'.format(base_motif_id, i + 1)
+
+        forward_offset = np.argmax(rng.gumbel(forward_weights))
+        reverse_offset = np.argmax(rng.gumbel(reverse_weights))
+
+        trunc_pwm = pwm[forward_offset:-reverse_offset, :] if reverse_offset > 0 \
+            else pwm[forward_offset:, :]
+        
+        if rc[i]:
+            trunc_pwm = np.flip(trunc_pwm)
+        trunc_pwm /= np.sum(trunc_pwm, axis=1, keepdims=True)
+        if trunc_pwm.shape[0] < min_width:
+            continue
+        
+        write_pfm(motif_id, trunc_pwm, nsites[i], forward_offset, reverse_offset, rc[i], out_fh, rng)
+        i += 1
+
+def simulate_periodic_motifs(base_motif_id, pwm, out_fh, exp_n=5., min_width=4, min_sites=20,
+                             max_sites=100, padding_p=.5, bg_alpha=100., rng=np.random.default_rng()):
+    entr = pwm.copy()
+    entr[entr > 0] *= np.log2(entr[entr > 0])
+    p = -np.sum(entr, axis=1) / 2 + 1e-10
+    forward_weights = np.cumsum(np.log(np.insert(p, 0, 1.))) + np.log1p(np.append(-p, 0.))
+    reverse_weights = np.cumsum(np.log(np.insert(np.flip(p), 0, 1.))) + \
+        np.log1p(np.append(-np.flip(p), 0.))
+    
+    n = rng.poisson(exp_n)
+    rc = rng.binomial(1, .5, size=n).astype(bool)
+    offset_dir = rng.binomial(1, .5, size=n).astype(bool)
+    nsites = rng.integers(min_sites, max_sites + 1, size=n)
+
+    i = 0
+    while i < n:
+        motif_id = '{}-{}'.format(base_motif_id, i + 1)
+
+        if offset_dir[i]:
+            forward_offset = np.argmax(rng.gumbel(forward_weights))
+            reverse_offset = -rng.geometric(padding_p) + 1
+        else:
+            forward_offset = -rng.geometric(padding_p) + 1
+            reverse_offset = np.argmax(rng.gumbel(reverse_weights))
+            
+        trunc_pwm = np.empty((pwm.shape[0] - forward_offset - reverse_offset, 4), dtype=np.float64)
+        if offset_dir[i]:
+            if reverse_offset == 0:
+                trunc_pwm[:, :] = pwm[forward_offset:, :]
+            else:
+                trunc_pwm[:reverse_offset, :] = pwm[forward_offset:, :]
+                for j in range(reverse_offset, 0):
+                    trunc_pwm[j, :] = rng.dirichlet([bg_alpha] * 4)
+        else:
+            if reverse_offset == 0:
+                trunc_pwm[-forward_offset:, :] = pwm
+            else:
+                trunc_pwm[-forward_offset:, :] = pwm[:-reverse_offset, :]
+            for j in range(-forward_offset):
+                trunc_pwm[j, :] = rng.dirichlet([bg_alpha] * 4)
+                    
+        if rc[i]:
+            trunc_pwm = np.flip(trunc_pwm)
+        trunc_pwm /= np.sum(trunc_pwm, axis=1, keepdims=True)
+        if trunc_pwm.shape[0] < min_width:
+            continue
+
+        write_pfm(motif_id, trunc_pwm, nsites[i], forward_offset, reverse_offset, rc[i], out_fh, rng)
+        i += 1
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Simulate motifs from base pwms')
+    parser.add_argument('base_pwms', type=str)
+    parser.add_argument('--exp-n', type=float, default=5.)
+    parser.add_argument('--min-width', type=int, default=4)
+    parser.add_argument('--min-sites', type=int, default=20)
+    parser.add_argument('--max-sites', type=int, default=100)
+    parser.add_argument('--padding-p', type=float, default=.5)
+    parser.add_argument('--bg-alpha', type=float, default=100.)
+    parser.add_argument('--seed', type=int, default=None)
+    parser.add_argument('--out', type=str, default='motifs.transfac')
+    args = parser.parse_args()
+
+    rng = np.random.default_rng(args.seed)
+    base_pwms, metadata = parse_tf(args.base_pwms)
+    with open(args.out, 'w') as f:
+        for motif_id, pwm in base_pwms.items():
+            if metadata[motif_id]['PD'] == 0:
+                simulate_aperiodic_motifs(motif_id, pwm, f, args.exp_n, args.min_width,
+                                           args.min_sites, args.max_sites, rng)
+            else:
+                simulate_periodic_motifs(motif_id, pwm, f, args.exp_n, args.min_width,
+                                         args.min_sites, args.max_sites, args.padding_p,
+                                         args.bg_alpha, rng)
